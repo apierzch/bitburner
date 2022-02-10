540407747584a8c051ed2ef48f652b25ac0aaf6f
Adam Pierzchała <adam.pierzchala@talkie.ai>
2022-02-10 21:00:23 +0100

    fix n00dles name

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { NS } from 'bitburner';
 
 export async function main(ns: NS) {
   ns.tprint("Hacking n00dles");
-  await ns.hack("n000dles");
-  await ns.weaken("n000dles");
-  await ns.grow("n000dles");
-  await ns.weaken("n000dles");
+  await ns.hack("n00dles");
+  await ns.weaken("n00dles");
+  await ns.grow("n00dles");
+  await ns.weaken("n00dles");
   ns.tprint("Hacking n00dles done");
 }
